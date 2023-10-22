20649cfe1a3da44ad02c5f4ff00677c20cf2e781
Huypham07 <huyhochoi541@gmail.com>
2023-10-23 04:49:14 +0700

    translate GUI
    use API google

---
+++ Dictionary/src/GUI/DictionaryGUI.java
@@ -0,0 +1,162 @@
+package GUI;
+
+import javax.swing.*;
+import GUI.ControlPanel.LookupGUI;
+import Dict.DictionaryManagement;
+import GUI.ControlPanel.TranslateGUI;
+import java.awt.CardLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class DictionaryGUI extends javax.swing.JFrame {
+    private static CardLayout cardLayout;
+    
+    private DictionaryManagement dictionaryManagement;
+    
+    private LookupGUI lookupGUI;
+    private TranslateGUI translateGUI;
+    
+    private static ArrayList<String> Listfuntion = new ArrayList<>();
+    
+    public DictionaryGUI() {
+        initComponents();
+        init();
+    }
+    
+    private void init() {
+        ImageIcon iconApp = new ImageIcon("src/data/img/logo.png");
+        setIconImage(iconApp.getImage());
+        
+        dictionaryManagement = new DictionaryManagement();
+        dictionaryManagement.insertFromFile();
+        
+        cardLayout = new CardLayout(0, 0);
+        
+        lookupGUI = new LookupGUI();
+        lookupGUI.setDictionay(dictionaryManagement);
+        
+        translateGUI = new TranslateGUI();
+        
+        workPanel.setLayout(cardLayout);
+        workPanel.add("lookup",lookupGUI);
+        workPanel.add("translate", translateGUI);
+        
+        Listfuntion.add("lookup");
+        Listfuntion.add("translate");
+        
+    }
+    
+    public static void change(int select) {
+        CardLayout c = (CardLayout)(workPanel.getLayout());
+        c.show(workPanel, Listfuntion.get(select));  
+    }
+    
+    
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        button1 = new GUI.roundComponent.Button();
+        controlpanel = new GUI.ControlPanel.controlPanel();
+        resultPanel1 = new GUI.ControlPanel.resultPanel();
+        workPanel = new javax.swing.JPanel();
+
+        button1.setText("button1");
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Dictionary");
+        setBackground(new java.awt.Color(245, 245, 245));
+        setResizable(false);
+
+        workPanel.setBackground(new java.awt.Color(255, 255, 255));
+        workPanel.setOpaque(false);
+
+        javax.swing.GroupLayout workPanelLayout = new javax.swing.GroupLayout(workPanel);
+        workPanel.setLayout(workPanelLayout);
+        workPanelLayout.setHorizontalGroup(
+            workPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 610, Short.MAX_VALUE)
+        );
+        workPanelLayout.setVerticalGroup(
+            workPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 570, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout resultPanel1Layout = new javax.swing.GroupLayout(resultPanel1);
+        resultPanel1.setLayout(resultPanel1Layout);
+        resultPanel1Layout.setHorizontalGroup(
+            resultPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(resultPanel1Layout.createSequentialGroup()
+                .addGap(95, 95, 95)
+                .addComponent(workPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(95, Short.MAX_VALUE))
+        );
+        resultPanel1Layout.setVerticalGroup(
+            resultPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(resultPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(workPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(controlpanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(50, 50, 50)
+                .addComponent(resultPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(controlpanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(resultPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    public static void main(String args[]) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(DictionaryGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(DictionaryGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(DictionaryGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(DictionaryGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new DictionaryGUI().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private GUI.roundComponent.Button button1;
+    private GUI.ControlPanel.controlPanel controlpanel;
+    private GUI.ControlPanel.resultPanel resultPanel1;
+    private static javax.swing.JPanel workPanel;
+    // End of variables declaration//GEN-END:variables
+}
